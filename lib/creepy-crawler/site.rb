8f121f95d84d2c4a18cd5ceaaed14991ac07dd6c
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-03 09:42:53 -0400

    completed the adding of pages and relationships to neo4j

--- lib/creepy-crawler/site.rb
+++ lib/creepy-crawler/site.rb
@@ -24,6 +24,7 @@ module Creepycrawler
       @options = options
       # add the initial url to our crawl queue
       @crawl_queue = [@url] 
+      @neo4j = Neography::Rest.new
       yield self if block_given?
     end
     
@@ -32,16 +33,15 @@ module Creepycrawler
         break if @crawl_queue.empty? or (!options[:max_page_crawl].nil? and @page_crawl_count >= options[:max_page_crawl])
         url = @crawl_queue.shift
         page = Page.new(url)
-        add_page_to_graph(page.url,true)
+        current_page_node = add_page_to_graph(page.url,true)
         # push each link onto our queue. todo: filter already visisted sites
         page.links.each do |link| 
-          # we only want to crawl current domain, so only push local links
-          crawl_queue.push(link) if local?(link)
+          # only push local links and links that do not already exist in the queue
+          crawl_queue.push(link) if local?(link) and !crawl_queue.push.include?(link)
 
-          # add the linked to page to our graph
-          add_page_to_graph(page.url,false)
+          current_link_node = add_page_to_graph(link,false)
           # create relationship from current
-          create_relationship_in_graph(page.url,link)
+          create_relationship_in_graph(current_page_node,current_link_node)
         end
         @page_crawl_count += 1
       end
@@ -55,13 +55,35 @@ module Creepycrawler
       return false
     end
 
+    def page_already_visisted?
+
+    end
+
     def add_page_to_graph(page,visited)
       # if page doesnt exist, add it to neo4j
       # if it exists and visited is false and passed in variable is true, update visited
+      begin
+        node = @neo4j.get_node_index("page", "url", page)
+      rescue Neography::NotFoundException => error 
+        node = nil
+      end
+      # node doesnt exist, create it 
+      if node.nil?
+        node = @neo4j.create_node("url" => page, "visited" => visited)
+        @neo4j.add_node_to_index("page", "url", page, node) unless node.nil?
+      else # node already existed, update visited property
+        # only want to flip nodes from not visted to visited. todo: find a better way to handle this 
+        if node[0]['data']['visited'] == false and visited == true 
+          @neo4j.set_node_properties(node, {"visited" => visited})
+        end
+      end
+
+      return node
     end
 
     def create_relationship_in_graph(from, to)
       # create relationship between nodes
+      @neo4j.create_relationship("links_to",from,to)
     end
   end
 end
