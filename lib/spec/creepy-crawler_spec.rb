4d9097abcc32ad7612996c3eca1d37bf9fba89bc
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-05 16:46:49 -0400

    use newer rspec syntax

--- lib/spec/creepy-crawler_spec.rb
+++ lib/spec/creepy-crawler_spec.rb
@@ -5,11 +5,12 @@ module Creepycrawler
     describe "#crawl" do
       it "should have a crawl convenience method to crawl the site and return a Site object" do
         result = Creepycrawler.crawl(RSPEC_URL,:graph_to_neo4j => false)
-        result.should be_an_instance_of Site
+        expect(result).to be_an_instance_of Site
       end 
+      
       it "should have a crawl convenience method that accepts options to crawl the site and return a Site object" do
         result = Creepycrawler.crawl(RSPEC_URL, :verbose => false, :graph_to_neo4j => false)
-        result.should be_an_instance_of Site
+        expect(result).to be_an_instance_of Site
       end 
     end
   end
