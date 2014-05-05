99df9c14ea40cb2ffcca91b02c11b52b64bee632
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-05 13:49:22 -0400

    for tests, don't try to write to neo4j database

--- lib/spec/creepy-crawler_spec.rb
+++ lib/spec/creepy-crawler_spec.rb
@@ -4,11 +4,11 @@ module Creepycrawler
   describe Creepycrawler do
     describe "#crawl" do
       it "should have a crawl convenience method to crawl the site and return a Site object" do
-        result = Creepycrawler.crawl(RSPEC_URL)
+        result = Creepycrawler.crawl(RSPEC_URL,:graph_to_neo4j => false)
         result.should be_an_instance_of Site
       end 
       it "should have a crawl convenience method that accepts options to crawl the site and return a Site object" do
-        result = Creepycrawler.crawl(RSPEC_URL, :dummy_option_1 => 'bar', :dummy_option_2 => 2)
+        result = Creepycrawler.crawl(RSPEC_URL, :verbose => false, :graph_to_neo4j => false)
         result.should be_an_instance_of Site
       end 
     end
