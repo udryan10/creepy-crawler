8e656f181dcc286dea25211823c2242b8ae8f786
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-05 16:47:47 -0400

    more testing

--- lib/spec/lib/site_spec.rb
+++ lib/spec/lib/site_spec.rb
@@ -5,6 +5,16 @@ module Creepycrawler
     before :each do
       @site = Site.new(RSPEC_URL)
     end
+
+    it "should accept options" do
+      @site = Site.new(RSPEC_URL, :foo => true)
+      expect(@site.options[:foo]).to be true
+    end
+
+    it "should allow the changing of default options" do
+      @site = Site.new(RSPEC_URL, :verbose => false)
+      expect(@site.options[:verbose]).to be false
+    end
   
     describe "#new" do
       it "should accept a url and return a site object" do
@@ -13,7 +23,7 @@ module Creepycrawler
     end
 
     describe "#local?" do
-      it "should corectly recognize a url local" do
+      it "should corectly recognize a local url" do
         expect(@site.local?("#{RSPEC_URL}/foo")).to be true
       end
 
