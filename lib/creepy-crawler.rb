08b2a601494e76afee2201dfa21cbafd804f80a5
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-06 14:57:42 -0400

    added ability to kick off crawl from command line

--- lib/creepy-crawler.rb
+++ lib/creepy-crawler.rb
@@ -6,9 +6,10 @@ require 'open-uri'
 require 'addressable/uri'
 require 'open_uri_redirections'
 require 'webrobots'
-
-# dynamically require all creepy-crawler/*.rb
-Dir[File.dirname(__FILE__) + '/creepy-crawler/*.rb'].each {|file| require file }
+require 'trollop'
+require_relative 'creepy-crawler/site'
+require_relative 'creepy-crawler/page'
+require_relative 'creepy-crawler/graph'
 
 module Creepycrawler
   # todo: on my local machine im hitting some openssl bug to where legitimate https sites are not validating the certificate.
@@ -36,4 +37,25 @@ module Creepycrawler
   def Creepycrawler.crawl(url, options = {})
     return Site.new(url, options).crawl
   end
+end
+
+
+# allow the initiating of a crawl from command line 
+if __FILE__==$0
+  # setup options
+  opts = Trollop::options do
+    opt :site, "url of site to crawl", :type => :string  # flag --site
+    opt :obey_robots, "Obey robots.txt disallow list"    # string --name <s>, default nil
+    opt :verbose, "Whether to print crawling information", :default => true
+    opt :max_page_crawl, "Maximum number of pages to crawl. Defaults to unlimited", :default => 0 
+    opt :graph_to_neo4j, "Whether pages should be written to graph database", :default => true
+  end
+  
+  Trollop::die :site, "Must specify a site to crawl" unless opts[:site]
+  opts[:max_page_crawl] = nil if opts[:max_page_crawl] == 0
+  
+  options_hash = {:obey_robots => opts[:obey_robots], :verbose => opts[:verbose], :max_page_crawl => opts[:max_page_crawl], :graph_to_neo4j => opts[:graph_to_neo4j]}
+ 
+  # start crawl
+  Creepycrawler.crawl(opts[:site], options_hash)
 end
