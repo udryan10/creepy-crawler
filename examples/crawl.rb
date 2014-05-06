19f910702be606e8c05f359d7179ccfbedb674ce
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-05 23:30:17 -0400

    added example script and output map

---
+++ examples/crawl.rb
@@ -0,0 +1,10 @@
+require '../lib/creepy-crawler'
+
+crawler = Creepycrawler.crawl("http://yahoo.com", :max_page_crawl => 100)
+puts "=" * 40
+puts "broken link list:"
+puts crawler.broken_links
+puts "=" * 40
+puts "number of visited pages:"
+puts crawler.page_crawl_count
+puts "=" * 40
